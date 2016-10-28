ce1592bd9beba27ae1d4fb027491bd1ecc675782
tihimsm <tihimsm@gmail.com>
2016-10-28 17:24:18 +0900

    スプラッシュ速度調整

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function splash(){
   $('#slide1').transition({
     opacity: 1,
     right: 0
-  }, 500, function(){
+  }, 1000, function(){
     $(this).transition({
       opacity: 0
     }, 800, function(){
@@ -15,11 +15,56 @@ function splash(){
     $('#slide2').transition({
       opacity: 1,
       left: 0
-    }, 500, function(){
+    }, 800, function(){
       $(this).transition({
         opacity: 0
       }, 800 ,function(){
         $(this).remove();
+      });
+      $('#slide3').transition({
+        opacity: 1,
+        top: 0
+      }, 600, function(){
+        $(this).transition({
+          opacity: 0
+        }, 800 ,function(){
+          $(this).remove();
+        });
+        $('#slide4').transition({
+          opacity: 1,
+          bottom: 0
+        }, 500, function(){
+          $(this).transition({
+            opacity: 0
+          }, 800 ,function(){
+            $(this).remove();
+          });
+          $('#slide5').transition({
+            opacity: 1,
+            bottom: 0,
+            left: 0
+          }, 400, function(){
+            $(this).transition({
+              opacity: 0
+            }, 800 ,function(){
+              $(this).remove();
+            });
+            $('#slide6').transition({
+              opacity: 1,
+              top: 0,
+              right: 0
+            }, 300, function(){
+              $(this).transition({
+                opacity: 0
+              }, 1500 ,function(){
+                $(this).remove();
+              });
+              $('#slide7').transition({
+                opacity: 1,
+              }, 4000);
+            });
+          });
+        });
       });
     });
   });
