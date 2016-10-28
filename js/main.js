6a5cebe881767b018d010f4bceb9f6f85aa31757
tihimsm <tihimsm@gmail.com>
2016-10-28 16:42:06 +0900

    スプラッシュ基盤作成&sass環境構築

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 $(window).load(function(){
-
+  splash();
 });
+
+function splash(){
+  $('#slide1').transition({
+    opacity: 1,
+    right: 0
+  }, 500, function(){
+    $(this).transition({
+      opacity: 0
+    }, 800, function(){
+      $(this).remove();
+    });
+    $('#slide_cont').append("<div id='slide2' class='slide_pict'></div>");
+    $('#slide2').transition({
+      opacity: 1,
+      left: 0
+    }, 500, function(){
+      $(this).transition({
+        opacity: 0
+      }, 800 ,function(){
+        $(this).remove();
+      });
+    });
+  });
+}
